a889d520ea9353953c7f6e7cb84c7bb210a656d4
seonjaena <75157491+seonjaena@users.noreply.github.com>
2025-06-03 16:24:34 +0900

    Delete function_app.py

--- function_app.py
+++
@@ -1,9 +0,0 @@
-import logging
-import azure.functions as func
-import pyodbc
-import os
-
-def main(changes: str, context: func.Context):
-    logging.warning('hello world')  # will print a message to the console
-    logging.info('hello world')  # will not print anything
-    print("hello world")
