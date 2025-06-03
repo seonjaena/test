538c3fcbadd20687fed2ba71209b6ceac3bd5057
seonjaena <75157491+seonjaena@users.noreply.github.com>
2025-06-03 16:06:29 +0900

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -3,7 +3,7 @@ import azure.functions as func
 import pyodbc
 import os
 
-def main(changes: func.SqlRowList) -> None:
+def main(changes: str, context: func.Context):
     logging.warning('hello world')  # will print a message to the console
     logging.info('hello world')  # will not print anything
     print("hello world")
