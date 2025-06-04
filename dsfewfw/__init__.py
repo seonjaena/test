5351813cf8161dd0254b52e9f4e73745a96b28ad
seonjaena <75157491+seonjaena@users.noreply.github.com>
2025-06-04 16:18:24 +0900

    Update __init__.py

--- dsfewfw/__init__.py
+++ dsfewfw/__init__.py
@@ -2,4 +2,6 @@ import json
 import logging
 
 def main(changes):
+    for change in changes:
+        logging.info("method: %s", getattr(change, 'operation', None))
     logging.info("SQL Changes: %s", json.loads(changes))
