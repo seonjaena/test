bd227a51669c4bf29317647a4950f081b7c1fc36
seonjaena <75157491+seonjaena@users.noreply.github.com>
2025-06-04 17:06:18 +0900

    Update __init__.py

--- dsfewfw/__init__.py
+++ dsfewfw/__init__.py
@@ -2,19 +2,6 @@ import json
 import logging
 import azure.functions as func
 
-def main(req: func.HttpRequest, product: func.Out[func.SqlRow]) -> func.HttpResponse:
-    try:
-        body = json.loads(req.get_body())
-        row = func.SqlRow.from_dict(body)
-        product.set(row)
-
-        return func.HttpResponse(
-            body=req.get_body(),
-            status_code=201,
-            mimetype="application/json"
-        )
-    except Exception as e: 
-        logger.error(e)
-
-#    logging.info(json.dumps(changes.__dict__, default=lambda o: o.to_json()))
-#    logging.info("SQL Changes: %s", json.loads(changes))
+def main(changes):
+    logging.info(changes)
+    logging.info("SQL Changes: %s", json.loads(changes))
