4567149518482215b76f5959fcdd236ce580598d
seonjaena <75157491+seonjaena@users.noreply.github.com>
2025-06-04 16:28:02 +0900

    Update __init__.py

--- dsfewfw/__init__.py
+++ dsfewfw/__init__.py
@@ -4,5 +4,5 @@ import azure.functions as func
 
 def main(changes: func.SqlRowList):
     for change in changes:
-        logging.info("test data: %s", json.loads(change))
+        logging.info("test data: %s", change)
     logging.info("SQL Changes: %s", json.loads(changes))
