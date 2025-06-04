c1027caec651d8e4b9a38f9d7f568ab926d13dc7
seonjaena <75157491+seonjaena@users.noreply.github.com>
2025-06-04 16:34:27 +0900

    Update __init__.py

--- dsfewfw/__init__.py
+++ dsfewfw/__init__.py
@@ -2,6 +2,13 @@ import json
 import logging
 import azure.functions as func
 
-def main(changes: func.SqlRowList):
-    logging.info(json.dumps(changes.__dict__, default=lambda o: o.to_json()))
+def main(req: func.HttpRequest, data: func.Out[func.SqlRow]) -> func.HttpResponse:
+    body = json.loads(req.get_body())
+    row = func.SqlRow.from_dict(body)
+    logging.info(body)
+    logging.info(row)
+    logging.info(data0
+
+
+#    logging.info(json.dumps(changes.__dict__, default=lambda o: o.to_json()))
 #    logging.info("SQL Changes: %s", json.loads(changes))
