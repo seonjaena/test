96663744514c3fa3f257eee47fd279ba13f93464
seonjaena <75157491+seonjaena@users.noreply.github.com>
2025-06-04 17:17:19 +0900

    Update __init__.py

--- dsfewfw/__init__.py
+++ dsfewfw/__init__.py
@@ -3,8 +3,8 @@ import logging
 import azure.functions as func
 
 def main(changes):
-    changes = json.loads(changes)
-    for change in changes: 
+    change_li = json.loads(changes)
+    for change in change_li: 
         print(change)
         if change['Operation'] == 0: 
             logging.info("THIS IS INSERT")
